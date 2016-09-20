c20c04ce8a96628494a1fda1fb9c0f211882b867
Kurt Weber <weberkrt@msn.com>
2016-09-19 20:09:48 -0400

    Homework Slide 22

---
+++ DictionaryAnimalSlide_2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryAnimalSlide_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize varibales
+            string animalName;
+
+            // User inpurt
+            Console.Write("Please enter an animal to search: ");
+            animalName = Console.ReadLine();
+            Console.WriteLine();
+            
+            // Set up dictionary with animal and numebr of legs
+            Dictionary<string, int> animals = new Dictionary<string, int>();
+            animals.Add("Lion", 4);
+            animals.Add("Spider", 6);
+            animals.Add("Bear",4);
+            animals.Add("Centipede", 100);
+            animals.Add("Eagle", 2);
+            animals.Add("Monkey", 4);
+
+            if (animals.ContainsKey(animalName))
+            {
+                int value = animals[animalName];
+                Console.WriteLine("The " + animalName + " has " + value + " legs.\n");
+            }
+
+
+        }
+    }
+}

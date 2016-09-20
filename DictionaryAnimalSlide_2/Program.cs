3d4f2692231facecb8d17aaa88a860a5b49fe26b
KurtWeber <weberkrt@msn.com>
2016-09-20 09:04:10 -0400

    New program

--- DictionaryAnimalSlide_2/Program.cs
+++ DictionaryAnimalSlide_2/Program.cs
@@ -21,19 +21,21 @@ namespace DictionaryAnimalSlide_2
             // Set up dictionary with animal and numebr of legs
             Dictionary<string, int> animals = new Dictionary<string, int>();
             animals.Add("Lion", 4);
-            animals.Add("Spider", 6);
+            animals.Add("Spider", 8);
             animals.Add("Bear",4);
             animals.Add("Centipede", 100);
             animals.Add("Eagle", 2);
             animals.Add("Monkey", 4);
-
+            // Print out animal with number of legs
             if (animals.ContainsKey(animalName))
             {
                 int value = animals[animalName];
                 Console.WriteLine("The " + animalName + " has " + value + " legs.\n");
             }
-
-
+            else // If animal not found
+            {
+                Console.WriteLine(animalName + " not found in dictionary.\n");
+            }
         }
     }
 }
